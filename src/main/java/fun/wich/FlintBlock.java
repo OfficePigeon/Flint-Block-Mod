728244be70684d11290c5a3749190e04865c2ef2
OfficePigeon <ava@wich.fun>
2026-01-04 22:27:45 -0500

    1.16

--- src/main/java/fun/wich/FlintBlock.java
+++ src/main/java/fun/wich/FlintBlock.java
@@ -6,35 +6,32 @@ import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.InteractionHand;
-import net.minecraft.world.ItemInteractionResult;
-import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.BaseFireBlock;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.gameevent.GameEvent;
 import net.minecraft.world.phys.BlockHitResult;
-import org.jetbrains.annotations.NotNull;
 
 public class FlintBlock extends Block {
 	public FlintBlock(Properties properties) { super(properties); }
-	@Override
-	protected @NotNull ItemInteractionResult useItemOn(ItemStack stack, BlockState state, Level level, BlockPos pos, Player player, InteractionHand hand, BlockHitResult hit) {
-		if (stack.is(FlintBlockMod.LIGHTS_FLINT)) {
+	@Override @SuppressWarnings("deprecation")
+	public InteractionResult use(BlockState blockState, Level level, BlockPos pos, Player player, InteractionHand hand, BlockHitResult hit) {
+		ItemStack stack = player.getItemInHand(hand);
+		if (stack.getItem().is(FlintBlockMod.LIGHTS_FLINT)) {
 			BlockPos blockPos = hit.getBlockPos().relative(hit.getDirection());
 			if (BaseFireBlock.canBePlacedAt(level, blockPos, player.getDirection())) {
 				level.playSound(player, blockPos, SoundEvents.FLINTANDSTEEL_USE, SoundSource.BLOCKS, 1.0f, level.getRandom().nextFloat() * 0.4f + 0.8f);
 				level.setBlock(blockPos, BaseFireBlock.getState(level, blockPos), 11);
-				level.gameEvent(player, GameEvent.BLOCK_PLACE, blockPos);
 				if (player instanceof ServerPlayer) {
 					CriteriaTriggers.PLACED_BLOCK.trigger((ServerPlayer)player, blockPos, stack);
-					stack.hurtAndBreak(1, player, LivingEntity.getSlotForHand(hand));
+					stack.hurtAndBreak(1, player, playerx -> playerx.broadcastBreakEvent(hand));
 				}
-				return ItemInteractionResult.SUCCESS;
+				return InteractionResult.SUCCESS;
 			}
 		}
-		return ItemInteractionResult.PASS_TO_DEFAULT_BLOCK_INTERACTION;
+		return InteractionResult.PASS;
 	}
 }
--- src/main/java/fun/wich/FlintBlockMod.java
+++ src/main/java/fun/wich/FlintBlockMod.java
@@ -1,38 +1,33 @@
 package fun.wich;
 
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.fabricmc.fabric.api.tag.TagRegistry;
 import net.minecraft.core.Registry;
-import net.minecraft.core.registries.BuiltInRegistries;
-import net.minecraft.core.registries.Registries;
-import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.tags.TagKey;
+import net.minecraft.tags.Tag;
 import net.minecraft.world.item.BlockItem;
-import net.minecraft.world.item.CreativeModeTabs;
+import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.material.MapColor;
+import net.minecraft.world.level.material.Material;
+import net.minecraft.world.level.material.MaterialColor;
 
 import java.util.function.Function;
 
 public class FlintBlockMod implements ModInitializer {
 	public static final String MOD_ID = "wich";
 
-	public static final TagKey<Item> LIGHTS_FLINT = TagKey.create(Registries.ITEM, ResourceLocation.fromNamespaceAndPath(MOD_ID, "lights_flint"));
+	public static final Tag<Item> LIGHTS_FLINT = TagRegistry.item(new ResourceLocation(MOD_ID, "lights_flint"));
 
-	public static final Block FLINT_BLOCK = register("flint_block", FlintBlock::new, Block.Properties.ofFullCopy(Blocks.STONE).mapColor(MapColor.COLOR_BLACK));
+	public static final Block FLINT_BLOCK = register("flint_block", FlintBlock::new, Block.Properties.of(Material.STONE, MaterialColor.COLOR_BLACK).strength(1.5F, 6.0F));
 	public static Block register(String name, Function<Block.Properties, Block> blockFactory, Block.Properties properties) {
-		return Registry.register(BuiltInRegistries.BLOCK, ResourceKey.create(Registries.BLOCK, ResourceLocation.fromNamespaceAndPath(MOD_ID, name)), blockFactory.apply(properties));
+		return Registry.register(Registry.BLOCK, new ResourceLocation(MOD_ID, name), blockFactory.apply(properties));
 	}
-	public static final Item FLINT_BLOCK_ITEM = register("flint_block", properties -> new BlockItem(FLINT_BLOCK, properties), new Item.Properties());
+	public static final Item FLINT_BLOCK_ITEM = register("flint_block", properties -> new BlockItem(FLINT_BLOCK, properties), new Item.Properties().tab(CreativeModeTab.TAB_BUILDING_BLOCKS));
 	public static Item register(String name, Function<Item.Properties, Item> itemFactory, Item.Properties properties) {
-		return Registry.register(BuiltInRegistries.ITEM, ResourceKey.create(Registries.ITEM, ResourceLocation.fromNamespaceAndPath(MOD_ID, name)), itemFactory.apply(properties));
+		return Registry.register(Registry.ITEM, new ResourceLocation(MOD_ID, name), itemFactory.apply(properties));
 	}
 
 	@Override
-	public void onInitialize() {
-		ItemGroupEvents.modifyEntriesEvent(CreativeModeTabs.BUILDING_BLOCKS).register(tab -> tab.accept(FLINT_BLOCK_ITEM));
-	}
+	public void onInitialize() { }
 }
